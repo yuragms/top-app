f9f9b334d61bd4fb3b4747803e1362d554fa821e
yuragms <yura.gms@gmail.com>
2023-03-22 13:51:37 +0200

    1

---
+++ interfaces/menu.interface.ts
@@ -0,0 +1,19 @@
+export interface PageItem {
+  alias: string;
+  title: string;
+  _id: string;
+  category: string;
+}
+
+export interface MenuItem {
+  _id: {
+    secondCategory: string;
+  };
+  pages: PageItem[];
+}
+// export interface FirstLevelMenuItem {
+//   route: string;
+//   name: string;
+//   icon: JSX.Element;
+//   id: TopLevelCategory;
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,77 @@
-import { useState } from 'react';
+// import axios from 'axios';
+// import { GetStaticProps } from 'next';
+// import { useState } from 'react';
+// import { Button, Htag, P, Rating, Tag } from '../components';
+// import { MenuItem } from '../interfaces/menu.interface';
+// import { withLayout } from '../layout/Layout';
+
+// function Home({ menu }: HomeProps): JSX.Element {
+//   const [rating, setRating] = useState<number>(4);
+
+//   return (
+//     <>
+//       <Htag tag="h2">Text</Htag>
+//       <Button appearance="primary" arrow="right">
+//         Кнопка
+//       </Button>
+//       <Button appearance="ghost" arrow="down">
+//         Кнопка
+//       </Button>
+//       <P size="l">Большой</P>
+//       <P>Средний</P>
+//       <P size="s">Маленький</P>
+//       <Tag size="s">Ghost</Tag>
+//       <Tag size="m" color="red">
+//         Red
+//       </Tag>
+//       <Tag size="s" color="green">
+//         Green
+//       </Tag>
+//       <Tag color="primary">Primary</Tag>
+//       <Rating rating={rating} isEditable setRating={setRating} />
+//       <ul>
+//         {menu.map((m) => (
+//           <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default withLayout(Home);
+
+// export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+//   const firstCategory = 0;
+//   const { data: menu } = await axios.post<MenuItem[]>(
+//     process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+//     { firstCategory }
+//   );
+//   return {
+//     props: {
+//       menu,
+//       firstCategory,
+//     },
+//   };
+// };
+
+// interface HomeProps extends Record<string, unknown> {
+//   menu: MenuItem[];
+//   firstCategory: number;
+// }
+
+import { GetStaticProps } from 'next';
+import React, { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
     <>
-      <Htag tag="h2">Text</Htag>
+      <Htag tag="h1">Заголовок</Htag>
       <Button appearance="primary" arrow="right">
         Кнопка
       </Button>
@@ -24,10 +88,36 @@ function Home(): JSX.Element {
       <Tag size="s" color="green">
         Green
       </Tag>
-      <Tag color="primary">Primary</Tag>
+      <Tag color="primary">Green</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
