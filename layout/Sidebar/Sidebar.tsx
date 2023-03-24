0e92422cbe58eefbbe004aaf560cac1effffb216
yuragms <yura.gms@gmail.com>
2023-03-24 20:51:36 +0200

    context73

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,12 @@
 import styles from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
 import cn from 'classnames';
+import { Menu } from '../Menu/Menu';
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}>Sidebar</div>;
+  return (
+    <div {...props}>
+      <Menu />
+    </div>
+  );
 };
