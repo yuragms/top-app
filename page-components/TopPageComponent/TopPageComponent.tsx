fcfa0da57835fab5ccfdee2ee86ee0fbd345eb59
yuragms <yura.gms@gmail.com>
2023-03-26 23:39:49 +0300

    products

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,23 @@
+import { Htag, Tag } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
+import styles from './TopPageComponent.module.css';
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортировка</span>
+      </div>
+    </div>
+  );
 };
