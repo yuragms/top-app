7a906501f14664d7eb56e525159f491bd379ee99
yuragms <yura.gms@gmail.com>
2023-03-28 21:25:44 +0300

    input89

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,6 @@
-// import axios from 'axios';
-// import { GetStaticProps } from 'next';
-// import { useState } from 'react';
-// import { Button, Htag, P, Rating, Tag } from '../components';
-// import { MenuItem } from '../interfaces/menu.interface';
-// import { withLayout } from '../layout/Layout';
-
-// function Home({ menu }: HomeProps): JSX.Element {
-//   const [rating, setRating] = useState<number>(4);
-
-//   return (
-//     <>
-//       <Htag tag="h2">Text</Htag>
-//       <Button appearance="primary" arrow="right">
-//         Кнопка
-//       </Button>
-//       <Button appearance="ghost" arrow="down">
-//         Кнопка
-//       </Button>
-//       <P size="l">Большой</P>
-//       <P>Средний</P>
-//       <P size="s">Маленький</P>
-//       <Tag size="s">Ghost</Tag>
-//       <Tag size="m" color="red">
-//         Red
-//       </Tag>
-//       <Tag size="s" color="green">
-//         Green
-//       </Tag>
-//       <Tag color="primary">Primary</Tag>
-//       <Rating rating={rating} isEditable setRating={setRating} />
-//       <ul>
-//         {menu.map((m) => (
-//           <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default withLayout(Home);
-
-// export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-//   const firstCategory = 0;
-//   const { data: menu } = await axios.post<MenuItem[]>(
-//     process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
-//     { firstCategory }
-//   );
-//   return {
-//     props: {
-//       menu,
-//       firstCategory,
-//     },
-//   };
-// };
-
-// interface HomeProps extends Record<string, unknown> {
-//   menu: MenuItem[];
-//   firstCategory: number;
-// }
-
 import { GetStaticProps } from 'next';
 import React, { useState } from 'react';
-import { Button, Htag, P, Rating, Tag } from '../components';
+import { Button, Htag, Input, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../interfaces/menu.interface';
@@ -90,6 +29,7 @@ function Home({ menu }: HomeProps): JSX.Element {
       </Tag>
       <Tag color="primary">Green</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <Input placeholder="тест" />
     </>
   );
 }
