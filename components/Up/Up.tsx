23bdbd513916718b0fb7f57db2513ae84a723289
yuragms <yura.gms@gmail.com>
2023-04-03 19:31:38 +0300

    dynamicIcon115

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -3,6 +3,7 @@ import styles from './Up.module.css';
 import UpIcon from './Up.svg';
 import { useScrollY } from '../../hooks/useScrollY';
 import { useEffect } from 'react';
+import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
 
 export const Up = (): JSX.Element => {
   const controls = useAnimation();
@@ -19,13 +20,12 @@ export const Up = (): JSX.Element => {
     });
   };
   return (
-    <motion.button
+    <motion.div
       animate={controls}
       className={styles.up}
-      onClick={scrollToTop}
       initial={{ opacity: 0 }}
     >
-      <UpIcon />
-    </motion.button>
+      <ButtonIcon appearance="primary" icon="up" onClick={scrollToTop} />
+    </motion.div>
   );
 };
