9c8bc6d86261e1e0574f485082a720986ebeea15
yuragms <yura.gms@gmail.com>
2023-04-05 12:04:03 +0300

    assesebilitySort123

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -11,20 +11,20 @@ export const Sort = ({
 }: SortProps): JSX.Element => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({ [styles.active]: sort == SortEnum.Rating })}
       >
         <SortIcon className={styles.sortIcon} />
         По рейтингу
-      </span>
-      <span
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({ [styles.active]: sort == SortEnum.Price })}
       >
         <SortIcon className={styles.sortIcon} />
         По цене
-      </span>
+      </button>
     </div>
   );
 };
